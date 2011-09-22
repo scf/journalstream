865fe9cf3fee7f0f1adbd56ff48d2186a2d47842
Nick Maloney <ngmaloney@gmail.com>
2011-09-22 15:03:55 -0400

    Added inline moderation form component to journalstream

--- templates/node--teaser-moderation.tpl.php
+++ templates/node--teaser-moderation.tpl.php
@@ -1,11 +1,13 @@
 <div id="node-<?php print $node->nid; ?>-moderation-form">
   <?php print render($moderation_form); ?>
-  <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-    <div class="content"<?php print $content_attributes; ?>>
+  <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix journalstream-content-wrapper"<?php print $attributes; ?>>
+    <div class="content journalstream-content"<?php print $content_attributes; ?>>
       <?php
         // We hide the comments and links now so that we can render them later.
         print render($content);
       ?>
+    <!-- Inline Moderation Form -->
+    <?php print render($inline_moderation_form); ?>
     </div><!-- /.content -->
   </div><!-- /#node-<?php print $node->nid; ?> -->
 </div><!--<?php print $node->nid; ?>-moderation-form -->
